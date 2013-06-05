8d63c6bcec537ab25996610d0188066b4c382929
sdriver312 <gsteers@gmail.com>
2013-06-05 14:36:58 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JacquiFiles::Application.config.secret_token = '09afb21e9fea9e29eb3c5fdc7893c26358e657552799ac45cfb6c3b0d8638831c9180efb7cc9456e6d0a6fbec93fde37479c752fbf77f9572b0f1cf3daf41218'
